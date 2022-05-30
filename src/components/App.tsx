5c090f8a11bbaa1938532f739eafa5923d8a42f2
István Korompai <istvan.korompai2@agcocorp.com>
2022-05-30 08:35:05 +0000

    split out the canvas to separate component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,31 +1,11 @@
 import { Fragment } from "react";
-import ColumnNode from "./ColumnNode";
-import { useRecoilValue } from "recoil";
-
-import { linksState, nodesStaticState } from "../utils/state";
-import NodeLink from "./NodeLink";
+import GraphCanvas from "./GraphCanvas";
 import AppBarDrawer from "./AppBarDrawer";
 
 function App() {
-  const links = useRecoilValue(linksState);
-  // const [isInitialized, setIsInitialized] = useState(false);
-  // const [nodes, setNodes] = useRecoilState(nodesStateFamily);
-  const nodes = useRecoilValue(nodesStaticState);
   return (
     <Fragment>
-      <svg
-        viewBox="0 0 4000 2000"
-        width="4000px"
-        height="2000px"
-        style={{ position: "absolute", top: "64px" }}
-      >
-        {links.map((link, idx) => {
-          return <NodeLink start={link[0]} end={link[1]} key={`link${idx}`} />;
-        })}
-        {nodes?.map((col, idx) => {
-          return <ColumnNode {...col} key={col.name} />;
-        })}
-      </svg>
+      <GraphCanvas />
       <AppBarDrawer />
     </Fragment>
   );
---
+++ src/components/GraphCanvas.tsx
@@ -0,0 +1,31 @@
+import { Fragment } from "react";
+import ColumnNode from "./ColumnNode";
+import { useRecoilValue } from "recoil";
+
+import { linksState, nodesStaticState } from "../utils/state";
+import NodeLink from "./NodeLink";
+
+function GraphCanvas() {
+  const links = useRecoilValue(linksState);
+  // const [isInitialized, setIsInitialized] = useState(false);
+  // const [nodes, setNodes] = useRecoilState(nodesStateFamily);
+  const nodes = useRecoilValue(nodesStaticState);
+
+  return (
+    <svg
+      viewBox="0 0 4000 2000"
+      width="4000px"
+      height="2000px"
+      style={{ position: "absolute", top: "64px" }}
+    >
+      {links.map((link, idx) => {
+        return <NodeLink start={link[0]} end={link[1]} key={`link${idx}`} />;
+      })}
+      {nodes?.map((col, idx) => {
+        return <ColumnNode {...col} key={col.name} />;
+      })}
+    </svg>
+  );
+}
+
+export default GraphCanvas;
