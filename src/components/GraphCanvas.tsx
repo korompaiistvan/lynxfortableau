2323907bbcac0bc07378c944fb6ace6d41ed1122
István Korompai <istvan.korompai2@agcocorp.com>
2022-05-30 14:27:41 +0000

    added panning functionality to canvas

--- src/components/GraphCanvas.tsx
+++ src/components/GraphCanvas.tsx
@@ -1,4 +1,4 @@
-import { Fragment, WheelEvent, useState, useRef, useEffect } from "react";
+import { Fragment, WheelEvent, useState, useRef, useEffect, MouseEventHandler } from "react";
 import ColumnNode from "./ColumnNode";
 import { useRecoilValue } from "recoil";
 
@@ -14,6 +14,8 @@ function GraphCanvas() {
   const SVGRef = useRef<SVGSVGElement>(null);
   const [SVGBoundingBox, setSVGBoundingBox] = useState<DOMRect>();
   const [viewBox, setViewBox] = useState<ViewBox>([0, 0, 4000, 2000]);
+  const [isPanning, setIsPanning] = useState<boolean>(false);
+  const [pointerOrigin, setPointerOrigin] = useState<DOMPoint>();
 
   // holds the timer for setTimeout and clearInterval
   let movement_timer: NodeJS.Timeout | undefined = undefined;
@@ -48,9 +50,35 @@ function GraphCanvas() {
     setSVGBoundingBox(SVGRef.current.getBoundingClientRect());
   }
 
+  function getPointFromEvent(event: MouseEvent) {
+    const point = DOMPoint.fromPoint({ x: event.clientX, y: event.clientY });
+    const invertedSVGMatrix = SVGRef.current?.getScreenCTM()?.inverse();
+    return point.matrixTransform(invertedSVGMatrix);
+  }
+
+  const startPan: MouseEventHandler<SVGSVGElement> = (event) => {
+    if (!SVGBoundingBox) return;
+    setIsPanning(true);
+    setPointerOrigin(getPointFromEvent(event as unknown as MouseEvent));
+  };
+
+  const handlePan: MouseEventHandler<SVGSVGElement> = (event) => {
+    if (!isPanning) return;
+    event.preventDefault();
+    const pointerPosition = getPointFromEvent(event as unknown as MouseEvent);
+    const deltaX = pointerPosition.x - pointerOrigin!.x;
+    const deltaY = pointerPosition.y - pointerOrigin!.y;
+    setViewBox([viewBox[0] - deltaX, viewBox[1] - deltaY, viewBox[2], viewBox[3]]);
+  };
+
+  const endPan: MouseEventHandler<SVGSVGElement> = (event) => {
+    setIsPanning(false);
+  };
+
   useEffect(() => {
     storeDimensions();
   }, []);
+
   window.addEventListener("resize", () => {
     clearInterval(movement_timer);
     movement_timer = setTimeout(storeDimensions, RESET_TIMEOUT);
@@ -63,6 +91,9 @@ function GraphCanvas() {
       height="calc(100vh - 64px)"
       style={{ position: "relative", top: "56px" }}
       onWheel={handleZoom}
+      onMouseDown={startPan}
+      onMouseUp={endPan}
+      onMouseMove={handlePan}
       overflow="hidden"
       ref={SVGRef}
     >
