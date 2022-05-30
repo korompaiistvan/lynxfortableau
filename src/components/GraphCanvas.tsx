67b07ce6aab13d1118f8f8712b08f355037ef95a
István Korompai <istvan.korompai2@agcocorp.com>
2022-05-30 13:38:39 +0000

    added zoom to the svg element

--- src/components/GraphCanvas.tsx
+++ src/components/GraphCanvas.tsx
@@ -1,22 +1,70 @@
-import { Fragment } from "react";
+import { Fragment, WheelEvent, useState, useRef, useEffect } from "react";
 import ColumnNode from "./ColumnNode";
 import { useRecoilValue } from "recoil";
 
 import { linksState, nodesStaticState } from "../utils/state";
 import NodeLink from "./NodeLink";
 
+type ViewBox = [number, number, number, number];
+
 function GraphCanvas() {
   const links = useRecoilValue(linksState);
-  // const [isInitialized, setIsInitialized] = useState(false);
-  // const [nodes, setNodes] = useRecoilState(nodesStateFamily);
   const nodes = useRecoilValue(nodesStaticState);
+
+  const SVGRef = useRef<SVGSVGElement>(null);
+  const [SVGBoundingBox, setSVGBoundingBox] = useState<DOMRect>();
+  const [viewBox, setViewBox] = useState<ViewBox>([0, 0, 4000, 2000]);
+
+  // holds the timer for setTimeout and clearInterval
+  let movement_timer: NodeJS.Timeout | undefined = undefined;
+
+  // the number of ms the window size must stay the same size before the
+  // dimension state variable is reset
+  const RESET_TIMEOUT = 100;
+
+  function handleZoom(event: WheelEvent) {
+    if (!SVGBoundingBox) return;
+    const { clientX, clientY, deltaY } = event;
+
+    const zoomFactor = 0.1; // the percentage by which the viewBox will change every tick of the wheel
+    const zoomAmount = (deltaY / 100) * zoomFactor; // positive is zoom out, negative is zoom in
+
+    const xRel = (clientX - SVGBoundingBox.left) / SVGBoundingBox.width;
+    const vbXChange = zoomAmount * viewBox[2];
+    const newLeft = viewBox[0] - vbXChange * xRel;
+    const newWidth = viewBox[2] * (1 + zoomAmount);
+
+    const yRel = (clientY - SVGBoundingBox.top) / SVGBoundingBox.height;
+    const vbYChange = zoomAmount * viewBox[3];
+    const newTop = viewBox[1] - vbYChange * yRel;
+    const newHeight = viewBox[3] * (1 + zoomAmount);
+
+    const newViewBox: ViewBox = [newLeft, newTop, newWidth, newHeight];
+    setViewBox(newViewBox);
+  }
+
+  function storeDimensions() {
+    if (!SVGRef.current) return;
+    setSVGBoundingBox(SVGRef.current.getBoundingClientRect());
+  }
+
+  useEffect(() => {
+    storeDimensions();
+  }, []);
+  window.addEventListener("resize", () => {
+    clearInterval(movement_timer);
+    movement_timer = setTimeout(storeDimensions, RESET_TIMEOUT);
+  });
 
   return (
     <svg
-      viewBox="0 0 4000 2000"
-      width="4000px"
-      height="2000px"
-      style={{ position: "absolute", top: "64px" }}
+      viewBox={viewBox.join(" ")}
+      width="100%"
+      height="calc(100vh - 64px)"
+      style={{ position: "relative", top: "56px" }}
+      onWheel={handleZoom}
+      overflow="hidden"
+      ref={SVGRef}
     >
       {links.map((link, idx) => {
         return <NodeLink start={link[0]} end={link[1]} key={`link${idx}`} />;
